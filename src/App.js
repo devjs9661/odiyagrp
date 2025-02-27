8fe4d1b3079be9c940be6c02fbc3a7c3d5b6f8ac
devjs9661 <devjs9661@gmail.com>
2025-02-27 19:14:52 +1300

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import NavBar from "./components/Navbar"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Login } from './components/Login';
-import { Register } from './components/Register';
+//import { Login } from './components/Login';
+//import { Register } from './components/Register';
 import { Home } from './components/Home';
 import {About} from './components/About';
 
